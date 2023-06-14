f8f0274b72b3aab35f9e8976c107abd003ff426c
ariesdurian <136595722+ariesdurian@users.noreply.github.com>
2023-06-14 20:15:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,11 @@ import streamlit
 streamlit.title('My Parent New Health Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Chicken Biryani')
-streamlit.text('Chicken Inalamangan')
-streamlit.text('Sisig, Lumpia')
+🥣 🥗 🐔 🥑 🍞 🥣 🥗 🐔 🥑🍞
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
