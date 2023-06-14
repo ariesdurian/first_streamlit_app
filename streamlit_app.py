abaf863e9a6d388c7616a4940e5e89cbc651333d
ariesdurian <136595722+ariesdurian@users.noreply.github.com>
2023-06-14 20:13:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parent New Health Diner')
 
-streamlit.Header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Chicken Biryani')
 streamlit.text('Chicken Inalamangan')
 streamlit.text('Sisig, Lumpia')
